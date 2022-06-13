75cb4c38e9fb7fdfb842c776667e4ae8f804c50a
Ashad Nadeem Mahmudi <ashadnadeem@gmail.com>
2022-06-13 14:54:24 +0500

    Feat: Add API calls get post functions.
    Fill in the msg_api.js with get/post api calls.

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import React, {Component} from 'react';  
-import {Text, View, StyleSheet} from 'react-native';  
-  
+import {Text, View, StyleSheet, Button} from 'react-native'; 
+
+import {getPosts} from './src/api/msg_api.js';
+
 Props = {};  
-export default class App extends Component {  
-  render() {  
+  
+function chatFunction () {
+  // call api to get posts
+  getPosts().then(posts => {
+    console.log(posts);
+    alert(JSON.stringify(posts.data));
+  }).catch(err => {
+    console.log(err);
+  })
+}
+
+export default class App extends Component { 
+  render() {
     return (  
       <View>  
         <Text style={style.welcome}>Hello World</Text>
-        <Text style={style.banner}>Chat Bot App</Text>  
+        <Text style={style.banner}>Chat Bot App</Text>
+        <Button title="Start Chat" onPress={chatFunction} />
       </View>  
     );  
   }  
--- src/api/msg_api.js
+++ src/api/msg_api.js
@@ -1,1 +1,33 @@
 // API call for messages
+import axios from "axios";
+
+const baseUrl = 'https://jsonplaceholder.typicode.com';
+
+export async function getPosts() {
+    console.log("working");
+    var url = baseUrl + "/users/1";
+    try {
+      var response = await axios.get(url);
+    } catch (error) {
+      // handle error
+      alert(error.message);
+    }
+    return response;
+  };
+ 
+  export async function postData (){
+    axios
+      .post('https://jsonplaceholder.typicode.com/posts', {
+        title: 'foo',
+        body: 'bar',
+        userId: 1,
+      })
+      .then(function (response) {
+        // handle success
+        alert(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        // handle error
+        alert(error.message);
+      });
+  };
