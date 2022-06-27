0c08ce9500e33bb2c3acfc1a704d6c3e6b37a3cc
Ashad Nadeem Mahmudi <ashadnadeem@gmail.com>
2022-06-27 16:38:11 +0500

    Feat: Conversation with bot API
    Converse with api, send and receive message from ngrok server.

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-import React, {Component} from 'react';  
-import {Text, View, StyleSheet, Button} from 'react-native';
-// import styles2 from './chat_screen.css';
-
+import React, {Component, useState} from 'react';  
+import {Text, View, ScrollView, FlatList, StyleSheet, Button, TextInput, KeyboardAvoidingView} from 'react-native';
+import InvertibleScrollView from 'react-native-invertible-scroll-view';
 // import material ui icons
 import { Icon } from "@rneui/themed";
 // import Api
-import {getPosts} from './src/api/msg_api.js';
+import {getPosts, postData} from './src/api/msg_api.js';
 
-Props = {};  
+Props = {};
+
   
 function chatFunction () {
   // call api to get posts
@@ -42,56 +42,131 @@ const MyFunctionalComponent = () =>{
 // Header Component
 const Header = () => {
   return (
-    <View style={style.header}>
+    <View style={style().header}>
       <View style={{flexDirection:'row'}}>
-        <Icon name='arrow-back' size={32} color='white' style={style.logo}/>
-        <Icon name='account-circle' size={40} color='white' style={style.logo}/>
+        <Icon name='arrow-back' size={32} color='white' style={style().logo}/>
+        <Icon name='account-circle' size={40} color='white' style={style().logo}/>
         <View>
-          <Text style={style.headerText}>Chatbot</Text>
-          <Text style={style.headerOnline}>Online</Text>
+          <Text style={style().headerText}>Chatbot</Text>
+          <Text style={style().headerOnline}>Online</Text>
         </View>
       </View>
       <View style={{flexDirection:"row"}}>
-        <Icon name='video-call' size={32} color='white' style={style.logo}/>
-        <Icon name='call' size={32} color='white' style={style.logo}/>
-        <Icon name='menu' size={32} color='white' style={style.logo}/>
+        <Icon name='video-call' size={32} color='white' style={style().logo}/>
+        <Icon name='call' size={32} color='white' style={style().logo}/>
+        <Icon name='options-vertical' type="simple-line-icon" size={32} color='white' style={style().logo}/>
       </View>
     </View>
   );
 }
 
-// Footer component
-const Footer = () => {
-  return (
-    <View>
-      <Text>{"In progress"}</Text>
+// Message component
+const Message = (props) =>{
+  return(
+    // Add styling here. msgLen
+    // <View style={[styles.default,props.backgroundColor && {backgroundColor:props.backgroundColor}]}>
+    <View style={style({server: props.msg.server}).message}>
+      <Text style={style().message_text}>{props.msg.text}</Text>
+      <View style={{flexDirection:"row-reverse", paddingHorizontal:5, paddingBottom:5}}>
+        <Text>{"5:21pm"}</Text>
+      </View>
     </View>
+  );
+}
+
+// Body component
+const Body = (prop) => {
+  return(
+    // <>
+      <FlatList  
+        data = {[...prop.messages].reverse()}
+        inverted={true}
+        renderItem = {(text) => 
+          <Message msg={text.item}/>
+        }
+      /> 
+      //  {prop.messages.map((text) => <Message msg={text}/>)}
+    // </>
+  );
+}
+
+// Footer component
+const Footer = (props) => {
+  // text field
+  const [text, setText] = useState("");
+  // on change the text input set text
+  function onchange(value){setText(value)};
+  // Send the msg to server and get reply
+  async function sendMsg(){
+    if(text.length>0){
+      // send user text
+      const msg_thread = [...props.messages,{text:text, server:false}];
+      props.setMsg(msg_thread);
+      setText("");
+      // get reply
+      let reply;
+      postData({text}).then((data)=>{
+        reply = data;
+        // return servers response
+        console.log(reply + "=> typeof: "+ typeof reply);
+        // console.log(reply.data);
+        // console.log(typeof reply);
+        props.setMsg([...msg_thread, {text:reply, server:true}]);
+      });
+    }
+  }
+  return (
+    <View style={style().footer}>
+      <View style={style().inputText}>
+        <Icon name='emoji-emotions' size={32} color='grey' style={style().logo}/>
+        <TextInput style={{width:"75%"}} fontSize={18} value={text} onSubmitEditing={sendMsg} onChangeText={onchange} placeholder="Type a message."/>
+        <Icon name='photo-camera' size={32} color='grey' style={style().logo}/>
+      </View>
+      <Icon name={text.length>0? "send" : "mic"} backgroundColor={"#005e54"} iconStyle={{margin:10}} size={32} color='white' borderRadius={30} style={style().logo} onPress={sendMsg}/>
+    </View>
+  );
+}
+
+const MyApp = () =>{
+  const [msgs, setMsgs] = useState(
+    [{text:"Hello, is there anyone?", server: false},
+    {text:"Yes, How may I help you?", server: true},
+    {text:"I want to learn React-Native", server: false},
+    {text:"Go through documentation, stackoverflow", server: true}, 
+    {text:"I've already done that.", server: false},
+    {text:"Bas best, Lets plan then. :)", server: true},
+    {text:"Thankyou bhai for help", server: false},
+    {text:"Anytime :)", server: true},
+    {text:"Why would you not reply", server: false},
+    {text:"Auto-scroll works", server: true}
+    ]);
+  return(
+    <>
+      <Body messages={msgs}/>
+      <Footer messages={msgs} setMsg={setMsgs}/>
+    </>
   );
 }
 
 export default class App extends Component { 
   render() {
     return (  
-      <View style={{justifyContent:"space-between"}}>  
+      <KeyboardAvoidingView style={{backgroundColor:"grey",flex:1, justifyContent:"space-between"}}>  
         <Header />
-        <Text style={style.welcome}>Hello World</Text>
-        <Text style={style.banner}>Chat Bot App</Text>
-        <MyClassComponent />
-        <MyFunctionalComponent />
-        <Button title="Start Chat" onPress={chatFunction} />
-        <Footer/>
-      </View>  
+        <MyApp />
+      </KeyboardAvoidingView>  
     );  
   }  
 }
 
-const style = StyleSheet.create({
+const style = (props) => StyleSheet.create({
+// const style = StyleSheet.create({
   welcome: {  
     fontSize: 48,  
     textAlign: 'center',  
     margin: 10, 
     color: 'red',
-    fontWeight: 'bold',
+    fontWeight: 'bold'
   },
   banner:{
     fontSize: 32,
@@ -109,7 +184,7 @@ const style = StyleSheet.create({
   headerText : {
     fontSize: 28,
     color: 'white',
-    fontFamily: 'bold',
+    fontWeight: 'bold',
     paddingLeft: 10
   },
   headerOnline : {
@@ -121,5 +196,35 @@ const style = StyleSheet.create({
   logo: {
     marginHorizontal: 2,
     marginVertical: 8
+  },
+  message: {
+    backgroundColor: props?.server? "#fffff0": "#e1ffc7", 
+    shadowRadius:5,
+    width:"auto",
+    paddingHorizontal:5,
+    alignSelf: props?.server? "flex-start" : "flex-end",
+    margin: 5,
+    shadowColor: "transparent",
+    borderRadius: 10
+  },
+  message_text: {
+    fontSize: 20, 
+    fontWeight:"400", 
+    color:"black", 
+    paddingHorizontal:15, 
+    paddingVertical:5
+  },
+  inputText: {
+    flexDirection: "row",
+    backgroundColor: "white",
+    borderRadius: 25,
+    margin: 8,
+    width: "80%"
+  },
+  footer: {
+    height: 70, 
+    flexDirection:"row", 
+    justifyContent:"space-evenly", 
+    flexWrap:"wrap"
   }
 });
--- src/api/msg_api.js
+++ src/api/msg_api.js
@@ -1,9 +1,9 @@
 // API call for messages
 import axios from "axios";
 
-const baseUrl = 'https://jsonplaceholder.typicode.com';
+// const baseUrl = 'https://jsonplaceholder.typicode.com';
 
-// const baseUrl =  "https://3688-117-20-31-76.in.ngrok.io/todo";
+const baseUrl =  "https://4cdb-117-20-31-76.in.ngrok.io/conversation";
 
 export async function getPosts() {
     console.log("working");
@@ -17,19 +17,22 @@ export async function getPosts() {
     return response;
   };
  
-  export async function postData (){
-    axios
-      .post('https://jsonplaceholder.typicode.com/posts', {
-        title: 'foo',
-        body: 'bar',
-        userId: 1,
+  export async function postData (props){
+    let reply = "We will get back to you shortly.";
+    await axios
+      .post(baseUrl, {
+        user_input : props.text
       })
       .then(function (response) {
         // handle success
-        alert(JSON.stringify(response.data));
+        reply = response.data.data;
+        return reply;
+        // alert(JSON.stringify(response.data));
       })
       .catch(function (error) {
         // handle error
-        alert(error.message);
+        // alert(error.message);
+        return reply;
       });
+      return reply;
   };
