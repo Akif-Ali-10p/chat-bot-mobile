a93d2ccb3e1c8223ae565e8a7b4589ac6c9f86c1
maheensabahat <maheensabahat2000@gmail.com>
2022-07-07 15:24:54 +0500

    Add Google and Facebook Sign In

--- android/app/src/main/java/com/chatbotmobile/newarchitecture/components/MainComponentsRegistry.java
+++ android/app/src/main/java/com/chatbotmobile/newarchitecture/components/MainComponentsRegistry.java
@@ -1,4 +1,4 @@
-package com.awesomeproject.newarchitecture.components;
+package com.chatbotmobile.newarchitecture.components;
 
 import com.facebook.jni.HybridData;
 import com.facebook.proguard.annotations.DoNotStrip;
